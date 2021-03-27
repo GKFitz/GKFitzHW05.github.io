0f09b3835f467893453619ba7a4f168fe8a2ec12
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-03-27 19:37:28 -0400

    added new files, custom css, not bootstrap, set up the OL and li's and made the js outline

---
+++ script.js
@@ -0,0 +1,22 @@
+//create a day planner  
+//each hour needs to generate it own div; 10hours: 
+
+//Dev side
+//when page loads
+// Display current date
+// read data from local storage
+// if there is data from LS insert into HTML structure
+//even when there isnt data from LS we need color coding
+// addEventListeners to all save buttons
+
+
+
+
+//client
+// when user clicks save btn
+// saves the timeslot, and the text to LS list
+
+
+
+
+
