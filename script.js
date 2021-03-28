71ca1d1e06a89b0df16e1ced4a63cc14a9ad9dd5
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-03-27 20:35:33 -0400

    init function established, getting the current date printed on to the page

--- script.js
+++ script.js
@@ -3,7 +3,25 @@
 
 //Dev side
 //when page loads
+// this will load the other function we will use
+$(document).ready(init);
+function init() {
+    let d = getCurrentDate();
+
+   
+}
+
 // Display current date
+function getCurrentDate() {
+    let d = new Date();
+    let dString = d.toLocaleString();
+
+    $("p+p").text(dString);
+
+    return d;
+
+}
+
 // read data from local storage
 // if there is data from LS insert into HTML structure
 //even when there isnt data from LS we need color coding
